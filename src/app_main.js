0f8753e4c0a3e4512ab652f9806d8c358b994c46
Fabian Keller <github.100.fkeller@spamgourmet.com>
2019-07-28 14:54:04 +0200

    improved electron keyboard shortcut handling

--- src/app_main.js
+++ src/app_main.js
@@ -2,8 +2,9 @@
 'use strict';
 
 const electron = require('electron');
+const electronLocalshortcut = require('electron-localshortcut');
+
 const { app, BrowserWindow, globalShortcut, crashReporter, ipcMain, dialog } = electron;
-const fs = require('fs');
 const path = require('path');
 
 
@@ -18,6 +19,17 @@ function makeHttpAddress(url) {
 
 
 function createWindow() {
+
+  // As per https://github.com/electron/electron/issues/16521
+  // calling this before creating the browser window is a work-around
+  // for the bug that calling setMenu(null) doesn't have an effect.
+  // But this also disables all keyboard shortcuts...
+  electron.Menu.setApplicationMenu(null)
+
+  // Option to use local shortcuts mentioned here: https://electronjs.org/docs/tutorial/keyboard-shortcuts
+  // This doesn't work, because these local shortcuts are only possible
+  // if the menu is visible...
+
   let mainWindow = new BrowserWindow(
     {
       width: 1000,
@@ -30,7 +42,17 @@ function createWindow() {
       },
     }
   )
-  mainWindow.setMenu(null)    // disable default menu
+
+  // Currently setMenu(null) has the unfortunate behavior, that it makes its
+  // keyboard shortcuts (accelerators) unusable, but still shows the menu.
+  // => worst behavior...
+  // mainWindow.setMenu(null)    // disable default menu
+
+  // This would be an okay option: Allows to toggle the menu with ALT and
+  // all shortcuts are active even if hidden. However there is one big
+  // problem: It starts out visible => bad for end users...
+  // mainWindow.setAutoHideMenuBar(true);
+
   mainWindow.maximize()
   // mainWindow.ELECTRON_DISABLE_SECURITY_WARNINGS = true
   // mainWindow["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true"
@@ -46,11 +68,37 @@ function createWindow() {
 
   mainWindow.on("closed", () => mainWindow = null)
 
+  // State for devtools
   let devToolsOpened = false
   if (devToolsOpened) {
     mainWindow.webContents.openDevTools()
   }
+  mainWindow.webContents.on('devtools-opened', () => {
+    console.log("Detected devtools opened")
+    devToolsOpened = true;
+  });
+  mainWindow.webContents.on('devtools-closed', () => {
+    console.log("Detected devtools closed")
+    devToolsOpened = false;
+  });
 
+  // For now this seems to be the best work-around for Electron's shortcut mess:
+  // https://github.com/parro-it/electron-localshortcut
+  electronLocalshortcut.register(mainWindow, "CommandOrControl+Shift+I", () => {
+    console.log("Toggeling dev tools")
+    if (devToolsOpened) {
+      mainWindow.webContents.closeDevTools()
+    } else {
+      mainWindow.webContents.openDevTools()
+    }
+  });
+  electronLocalshortcut.register(mainWindow, "CommandOrControl+R", () => {
+    console.log("Reloading");
+    mainWindow.reload();
+  });
+
+  /*
+  // Global shortcuts suck, because the are active even if the window doesn't have focus.
   globalShortcut.register("CommandOrControl+Shift+I", () => {
     console.log("Toggeling dev tools")
     if (devToolsOpened) {
@@ -61,6 +109,7 @@ function createWindow() {
       devToolsOpened = true
     }
   })
+  */
 
   // Redirect hrefs to external browser:
   // - https://stackoverflow.com/a/32415579/1804173
@@ -89,4 +138,29 @@ app.on("activate", () => {
     createWindow()
   }
 })
+
+
+/*
+// Another work-around for the shortcut problem in Electron.
+// From: https://github.com/electron/electron/issues/1334#issuecomment-310920998
+// Works, but feels fairly tedious because of all the manual input comparison
+// and the lack of direct access to mainWindow.
+
+let devToolsOpened = false
+
+app.on('web-contents-created', function (event, wc) {
+  wc.on('before-input-event', function (event, input) {
+    console.log(input)
+    if (input.type == "keyDown" && input.key === 'I' && input.control && input.shift) {
+      if (devToolsOpened) {
+        wc.closeDevTools()
+        devToolsOpened = false
+      } else {
+        wc.openDevTools()
+        devToolsOpened = true
+      }
+    }
+  })
+})
+*/
 
