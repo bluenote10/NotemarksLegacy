40c1c9c58a62c33d31536b5013de7f514b506d60
Fabian Keller <github.100.fkeller@spamgourmet.com>
2019-11-22 13:14:17 +0100

    improved label style

--- src/LabelTree.tsx
+++ src/LabelTree.tsx
@@ -10,18 +10,35 @@ export interface LabelTreeProps {
 // https://spectrum.chat/solid-js/general/solid-js-watercooler~a36894a2-2ea2-4b1e-9e56-03ed0b3aef13?m=MTU1OTIyNzQ5MDYzMw==
 // https://www.barbarianmeetscoding.com/blog/2016/05/13/argument-destructuring-and-type-annotations-in-typescript
 
-function Label({name}: {name: string}) {
-  return <span class="tag is-dark">{name}</span>
+function Label(props: {
+    name: string,
+    count: number,
+  }) {
+
+  function onClick() {
+    console.log("clicked label:", props.name);
+  }
+
+  return (
+    <span
+      class="ui-label"
+      onclick={() => onClick()}
+    >
+      {props.name}
+      <span class="ui-label-count">
+        {props.count}
+      </span>
+    </span>);
 }
 
 export function LabelTree(props: LabelTreeProps) {
   console.log("rendering labeltree")
   return (
-    <div>
+    <div class="ui-label-column">
       <For each={(props.labels)}>{
         (label: LabelCount) =>
           <div>
-            <Label name={(`${label.name} (${label.count})`)}/>
+            <Label name={(label.name)} count={(label.count)}/>
           </div>
       }</For>
     </div>
